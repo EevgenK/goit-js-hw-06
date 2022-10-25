fbb8a3594e3f236df663efb889bb17fa7aea5a5c
Evhen Kulbachenko <45nalitay45@gmail.com>
2022-10-26 01:48:11 +0300

    task-02
    task-02 is done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const items = ingredients.map((option) => {
+  const itemEl = document.createElement("li");
+  itemEl.classList.add("ingredients-item");
+  itemEl.textContent = option;
+  return itemEl;
+});
+
+const listEl = document.querySelector("#ingredients");
+listEl.append(...items);
+
+console.log(listEl);
