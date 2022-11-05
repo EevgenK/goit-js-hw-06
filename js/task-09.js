3ada03b1ffa7e87ec6bcc00be032b2f66fc0949a
Evhen Kulbachenko <45nalitay45@gmail.com>
2022-11-05 20:41:36 +0200

    task-09
    task-09 is done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const refs = {
+  body: document.querySelector("body"),
+  button: document.querySelector(".change-color"),
+  text: document.querySelector(".color"),
+};
+const setNewBg = (e) => {
+  const randomColor = getRandomHexColor();
+  refs.body.style.backgroundColor = randomColor;
+  refs.text.innerHTML = randomColor;
+};
+refs.button.addEventListener("click", setNewBg);
