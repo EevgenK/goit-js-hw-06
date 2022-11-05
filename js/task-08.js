f470f71ecae5e11e7bcf0483063e8cc181cb8f3f
Evhen Kulbachenko <45nalitay45@gmail.com>
2022-11-05 19:48:03 +0200

    task-08
    task-08 is done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const {
+    elements: { email, password },
+  } = e.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Будь ласка, заповніть всі поля!");
+  }
+  const loginFormValue = { login: email.value, password: password.value };
+  console.log(loginFormValue);
+  e.currentTarget.reset();
+}
