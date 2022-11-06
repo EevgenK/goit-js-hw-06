c56b7f3091942a6070fba326cfb901b56fea1659
Evhen Kulbachenko <45nalitay45@gmail.com>
2022-11-06 03:07:34 +0200

    task-10
    task-10 is done

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,34 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const refs = {
+  input: document.querySelector("input"),
+  btnCreate: document.querySelector("[data-create]"),
+  btnDestroy: document.querySelector("[data-destroy]"),
+  boxesEl: document.querySelector("#boxes"),
+};
+
+let size = 30;
+const addItem = () =>
+  `<div style="background-color: ${getRandomHexColor()}; width: ${size}px;  height: ${size}px;"> </div>`;
+const items = ["box"];
+function createBoxes(amount) {
+  amount = refs.input.value;
+  for (let i = 0; i < amount; i++) {
+    const boxes = items.map((box) => addItem(box)).join("");
+    size += 10;
+    refs.boxesEl.insertAdjacentHTML("beforeend", boxes);
+    console.log(amount);
+  }
+}
+
+const destroyBoxes = () => {
+  refs.boxesEl.innerHTML = "";
+  return (size = 30);
+};
+
+refs.btnCreate.addEventListener("click", createBoxes);
+refs.btnDestroy.addEventListener("click", destroyBoxes);
